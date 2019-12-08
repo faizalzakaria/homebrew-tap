e895ba23d582e56fac34aabeaa6e2ddee218148e
faizalzakaria <fai@code3.io>
2019-12-08 22:36:34 +0800

    Brew formula update for octo version v1.0.0

--- Formula/octo.rb
+++ Formula/octo.rb
@@ -6,12 +6,12 @@ class Octo < Formula
   bottle :unneeded
 
   if OS.mac?
-    url "http://github.com/faizalzakaria/releases/download/v1.0.0/octo_1.0.0_Darwin_x86_64.tar.gz"
-    sha256 "7f6a923c639663435f858107367707e8abf62d9409e3e9534634fcfaaaf38767"
+    url "http://github.com/faizalzakaria/octo/releases/download/v1.0.0/octo_1.0.0_Darwin_x86_64.tar.gz"
+    sha256 "b6cd7a32efbca458b0f2d5434e7a7142d6aabaa3ce6355995b1b2a8f61a7c315"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "http://github.com/faizalzakaria/releases/download/v1.0.0/octo_1.0.0_Linux_x86_64.tar.gz"
-      sha256 "f00de302581bd08f1c79572f5c4a1c937a00ba3e7d69bd2e297c9be1d3cba95e"
+      url "http://github.com/faizalzakaria/octo/releases/download/v1.0.0/octo_1.0.0_Linux_x86_64.tar.gz"
+      sha256 "b87826c5d0dcd5f10dc3fed58dd8139c8eb262eb71c06070f16150c9576a0db9"
     end
   end
   
